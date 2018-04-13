83ac2f0ac3ebf004840bc9bba0834ea0c05d7e3b
Danvic <danvic96@hotmail.com>
2018-04-13 09:51:36 +0800

    Create tools folder

---
+++ Tools/StringUtillits/StringUtillits/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringUtillits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
