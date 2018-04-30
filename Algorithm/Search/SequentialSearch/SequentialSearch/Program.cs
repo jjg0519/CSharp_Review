5e0432e0a43729aaeedfd5c249999f3c927aa556
Lanesra <danvic96@hotmail.com>
2018-04-30 16:43:00 +0800

    add search module

---
+++ Algorithm/Search/SequentialSearch/SequentialSearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SequentialSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
